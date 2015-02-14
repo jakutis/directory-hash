a876cdebb22044886c8edecb367314d6a5fa22b6
Vytautas Jakutis <vytautas@jakut.is>
2015-02-14 22:41:40 +0200

    add spec: main produces no output

--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,12 @@
 import Test.Hspec
 import System.Exit
+import System.IO.Silently
+import Control.Exception
 import qualified DirectoryHash.Executable
 
 main = hspec $ do
   describe "main" $ do
     it "exits with success" $
       DirectoryHash.Executable.main `shouldThrow` (== ExitSuccess)
+    it "produces no output" $
+      capture_ (catch DirectoryHash.Executable.main ((\_ -> return ()) :: ExitCode -> IO ())) `shouldReturn` ""
