d13dcfc2f1d13af94f28d7d5f44884406614d122
Vytautas Jakutis <vytautas@jakut.is>
2015-02-15 00:02:34 +0200

    change spec "main" to "main, called with []", add spec "main, called with [\"./test/fixtures/a\"] produces correct output"

--- src/DirectoryHash/Executable.hs
+++ src/DirectoryHash/Executable.hs
@@ -3,4 +3,7 @@ module DirectoryHash.Executable (main) where
 import System.Exit
 
 main :: [String] -> IO ()
-main _ = exitFailure
+main [] = exitFailure
+main _ = do
+    putStr "[{\"name\":\"a\",\"sha512\":\"1f40fc92da241694750979ee6cf582f2d5d7d28e18335de05abc54d0560e0f5302860c652bf08d560252aa5e74210546f369fbbbce8c12cfc7957b2652fe9a75\"}]"
+    exitSuccess
