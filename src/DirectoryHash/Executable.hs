5111c1eb51c0bf1af2e658c53f95b31110bd9b8a
Vytautas Jakutis <vytautas@jakut.is>
2015-02-15 12:16:19 +0200

    feat(.Executable.main): add spec "main, called with ["./test/fixtures/empty"] produces correct output"

--- src/DirectoryHash/Executable.hs
+++ src/DirectoryHash/Executable.hs
@@ -12,12 +12,13 @@ main :: [String] -> IO ()
 main [] = exitFailure
 main (directoryName:_) = do
     files <- getDirectoryContents directoryName
-    let
-      filesWithoutDots = filter (\file -> file /= "." && file /= "..") files
-      name = head filesWithoutDots
-      in do
-        content <- Data.ByteString.Lazy.readFile $ directoryName ++ "/" ++ name
-        let
-          hash = showDigest $ sha512 content
-          in Data.ByteString.Lazy.putStr $ encode $ [Data.HashMap.Strict.fromList [("name", name), ("sha512", hash)]]
+    hashFiles directoryName (filter (\file -> file /= "." && file /= "..") files)
     exitSuccess
+
+hashFiles :: String -> [String] -> IO ()
+hashFiles directoryName [] = putStr "[]"
+hashFiles directoryName (name:_) = do
+    content <- Data.ByteString.Lazy.readFile $ directoryName ++ "/" ++ name
+    let
+      hash = showDigest $ sha512 content
+      in Data.ByteString.Lazy.putStr $ encode $ [Data.HashMap.Strict.fromList [("name", name), ("sha512", hash)]]
--- test/Main.hs
+++ test/Main.hs
@@ -1,8 +1,9 @@
-import Test.Hspec
-import System.Exit
-import System.IO.Silently
 import Control.Exception
 import qualified DirectoryHash.Executable
+import qualified System.Directory
+import System.Exit
+import System.IO.Silently
+import Test.Hspec
 
 main = hspec $ do
 
@@ -21,3 +22,8 @@ main = hspec $ do
     it "produces correct output" $
       let b = "[{\"sha512\":\"5267768822ee624d48fce15ec5ca79cbd602cb7f4c2157a516556991f22ef8c7b5ef7b18d1ff41c59370efb0858651d44a936c11b7b144c48fe04df3c6a3e8da\",\"name\":\"b\"}]"
         in capture_ (catch (DirectoryHash.Executable.main ["./test/fixtures/b"]) ((\_ -> return ()) :: ExitCode -> IO ())) `shouldReturn` b
+
+  describe "main, called with [\"./test/fixtures/empty\"]" $ do
+    it "produces correct output" $ do
+      System.Directory.createDirectoryIfMissing False "./test/fixtures/empty"
+      capture_ (catch (DirectoryHash.Executable.main ["./test/fixtures/empty"]) ((\_ -> return ()) :: ExitCode -> IO ())) `shouldReturn` "[]"
