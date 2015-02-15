58089383ffb3aeb83731aeea128c42ace0f7bba9
Vytautas Jakutis <vytautas@jakut.is>
2015-02-15 11:45:59 +0200

    refactor(.Executable.main): read content directly into lazy bytestring

--- src/DirectoryHash/Executable.hs
+++ src/DirectoryHash/Executable.hs
@@ -3,7 +3,6 @@ module DirectoryHash.Executable (main) where
 import Data.Digest.Pure.SHA
 import qualified Data.ByteString.Lazy.Char8
 import qualified Data.ByteString.Lazy
-import qualified Data.ByteString (readFile)
 import System.Directory
 import System.Exit
 
@@ -15,8 +14,8 @@ main (directoryName:_) = do
       filesWithoutDots = filter (\file -> file /= "." && file /= "..") files
       name = head filesWithoutDots
       in do
-        content <- Data.ByteString.readFile $ directoryName ++ "/" ++ name
+        content <- Data.ByteString.Lazy.readFile $ directoryName ++ "/" ++ name
         let
-          hash = showDigest $ sha512 $ Data.ByteString.Lazy.fromStrict content
+          hash = showDigest $ sha512 content
           in putStr $ "[{\"name\":\"" ++ name ++ "\",\"sha512\":\"" ++ hash ++ "\"}]"
     exitSuccess
