58c25284965f743d2e7cb7f6b791c5e73fd9f5b3
Vytautas Jakutis <vytautas@jakut.is>
2015-02-16 00:57:29 +0200

    refactor(.Executable.main): ensure strict IO monad application, avoid too many open files error

--- src/DirectoryHash/Executable.hs
+++ src/DirectoryHash/Executable.hs
@@ -2,34 +2,34 @@ module DirectoryHash.Executable (main) where
 
 import Data.Aeson.Encode
 import qualified Data.ByteString.Lazy
-import qualified Data.ByteString.Lazy.Char8
 import Data.Digest.Pure.SHA
 import qualified Data.HashMap.Strict
 import Data.List
-import qualified System.Directory
-import System.Directory.Tree
+import System.Directory
 import System.Exit
 
 main :: [String] -> IO ()
 main [] = exitFailure
 main [directoryName] = do
-    files <- findFiles [] directoryName
+    files <- deepListFiles directoryName ""
     result <- mapM (hashFile directoryName) files
     Data.ByteString.Lazy.putStr $ encode $ result
     exitSuccess
 
-toFlatFileList :: String -> DirTree String -> [String]
-toFlatFileList prefix (File name _) = [prefix ++ "/" ++ name]
-toFlatFileList prefix (Dir name subdirs) = concat $ map (toFlatFileList $ prefix ++ "/" ++ name) subdirs
-
-findFiles :: [String] -> String -> IO [String]
-findFiles [] directoryName = do
-    (_ :/ (Dir dir contents)) <- readDirectory directoryName
-    return $ sort $ concat $ map (toFlatFileList "") contents
+deepListFiles :: String -> String -> IO [String]
+deepListFiles pathPrefix path = do
+    isDir <- doesDirectoryExist (pathPrefix ++ path)
+    case isDir of
+        True -> do
+            files <- getDirectoryContents (pathPrefix ++ path)
+            prefixedFiles <- mapM (\x -> return $ path ++ "/" ++ x) (filter (\x -> x /= "." && x /= "..") files)
+            allFiles <- mapM (deepListFiles pathPrefix) prefixedFiles
+            return $ sort $ concat allFiles
+        False -> return [path]
 
 hashFile :: String -> String -> IO (Data.HashMap.Strict.HashMap String String)
 hashFile directoryName name = do
     content <- Data.ByteString.Lazy.readFile $ directoryName ++ name
     let
       hash = showDigest $ sha512 content
-      in return $ Data.HashMap.Strict.fromList [("name", name), ("sha512", hash)]
+      in return $! Data.HashMap.Strict.fromList [("name", name), ("sha512", hash)]
